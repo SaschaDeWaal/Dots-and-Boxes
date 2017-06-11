bd584a416d9c8d18f81de9f4969f6d6636491769
beatnory <info@dreamfoxgames.com>
2017-06-11 20:44:26 +0200

    tussen versie

---
+++ Assets/Scripts/Managers/AccountManager.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using SimpleJSON;
+
+public class AccountManager : MonoBehaviour {
+
+	private const string SERVER = "http://studenthome.hku.nl/~Sascha.deWaal/land/api/";
+	private const int KEEP_ALIVE_TIME = 10; //<--min
+
+	public GameObject LoginUI;
+
+	private string sessionID = "";
+
+	public IEnumerator Login(string mail, string pass, System.Action<bool, string> result)
+	{
+		StopCoroutine(KeepLoginAlive());
+
+		WWWForm form = new WWWForm();
+		form.AddField("mail", mail);
+		form.AddField("pass", pass);
+
+		UnityWebRequest www = UnityWebRequest.Post(SERVER + "login.php", form);
+		yield return www.Send();
+
+		if (www.isError)
+		{
+			result(true, www.error);
+		}
+		else
+		{
+			JSONNode data = JSON.Parse(www.downloadHandler.text);
+			if (data["result"].Value.IndexOf("error") != -1)
+			{
+				result(true, data["data"].Value);
+			}
+			else
+			{
+				sessionID = data["sessionID"].Value;
+				StartCoroutine(KeepLoginAlive());
+				result(false, "");
+			}
+		}
+	}
+
+	private IEnumerator KeepLoginAlive()
+	{
+		while (true)
+		{
+			//wait for next
+			yield return new WaitForSeconds(KEEP_ALIVE_TIME * 60);
+
+			//Login to keep the session going.
+			WWWForm form = new WWWForm();
+			form.AddField("sessionID", sessionID);
+
+			UnityWebRequest www = UnityWebRequest.Post(SERVER + "keepAlive.php", form);
+			yield return www.Send();
+
+			if (www.isError)
+			{
+				LostConnection();
+			}
+			else
+			{
+				JSONNode data = JSON.Parse(www.downloadHandler.text);
+				if (data["result"].Value.IndexOf("error") != -1)
+				{
+					LostConnection();
+				}
+			}
+
+		}
+	}
+
+	private void LostConnection()
+	{
+		Debug.Log("Need to be logged in again");
+		LoginUI.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/UI/LoginUI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginUI : MonoBehaviour {
+
+	private const string WAITING_BUTTON_TEXT = "Loading....";
+	private const string LOGIN_BUTTON_TEXT = "Login";
+
+	public GameObject msgPanel;
+	public InputField mailInput;
+	public InputField passInput;
+	public Text buttonText;
+
+	public GameObject MatchMaker;
+
+	private bool waitingOnLogin = false;
+	
+
+	public void CreateAccount() {
+		Application.OpenURL("http://studenthome.hku.nl/~Sascha.deWaal/land/index.php?p=create");
+	}
+
+	public void Login() {
+		if (mailInput.text.Length < 3 && passInput.text.Length < 3) {
+			ShowMsg("Login error", "Please, enter a valid mail and password");
+		} else {
+			if (!waitingOnLogin) {
+				waitingOnLogin  = true;
+				buttonText.text = WAITING_BUTTON_TEXT;
+				StartCoroutine(Manager.Find<AccountManager>().Login(mailInput.text, passInput.text, LoginReady));
+			}
+		}
+	}
+
+	public void CloseMsgBox() {
+		if (msgPanel) {
+			msgPanel.SetActive(false);
+		} else {
+			Debug.LogWarning("msgPanel missing");
+		}
+	}
+
+	private void LoginReady(bool isError, string result){
+
+		waitingOnLogin  = false;
+		buttonText.text = LOGIN_BUTTON_TEXT;
+
+		if (isError) {
+			ShowMsg("Error", result);
+		} else {
+			MatchMaker.SetActive(true);
+			gameObject.SetActive(false);
+		}
+	}
+
+	private void ShowMsg(string title, string text) {
+		if (msgPanel) {
+			msgPanel.transform.Find("Title").GetComponent<Text>().text = title;
+			msgPanel.transform.Find("Text").GetComponent<Text>().text  = text;
+			msgPanel.SetActive(true);
+		} else {
+			Debug.LogWarning("msgPanel missing");
+		}
+	}
+}
